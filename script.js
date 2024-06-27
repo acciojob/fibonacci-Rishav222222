c8f4250dc890ab86d37e13be322197a818802368
Rishav222222 <160915777+Rishav222222@users.noreply.github.com>
2024-06-27 20:26:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ function fibonacci(num) {
 	let num1 = 0;
 	let num2 = 1;
 	let sum;
-	if(num1 === 1){
+	if(num === 1){
 		return num1;
 	}
-	else if(num2 === 1){
+	else if(num === 1){
 		return num2;
 	}else{
 		for(let i=3; i <= num; i++){
