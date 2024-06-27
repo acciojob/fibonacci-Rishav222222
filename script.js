7ee4d44131024fdbb4503b37b4575110540e4060
Rishav222222 <160915777+Rishav222222@users.noreply.github.com>
2024-06-27 20:22:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ function fibonacci(num) {
 	let num1 = 0;
 	let num2 = 1;
 	let sum;
-	if(num1 === 0){
+	if(num1 === 1){
 		return num1;
 	}
 	else if(num2 === 1){
 		return num2;
 	}else{
-		for(let i=0; i <= num; i++){
+		for(let i=3; i <= num; i++){
 			sum = num1 + num2;
 			num1 = num2;
 			num2 = sum;
